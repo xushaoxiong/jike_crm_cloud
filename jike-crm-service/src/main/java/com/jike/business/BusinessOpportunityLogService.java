39bfdb12d073996166c99a8499fb5bff8d38faa3
wangyingbin <498513411@qq.com>
2017-04-06 14:37:40 +0800

    日志

---
+++ jike-crm-service/src/main/java/com/jike/business/BusinessOpportunityLogService.java
@@ -0,0 +1,5 @@
+package com.jike.business;
+
+public interface BusinessOpportunityLogService {
+
+}
---
+++ jike-crm-service/src/main/java/com/jike/business/impl/BusinessOpportunityLogServiceImpl.java
@@ -0,0 +1,7 @@
+package com.jike.business.impl;
+
+import com.jike.business.BusinessOpportunityLogService;
+
+public class BusinessOpportunityLogServiceImpl implements BusinessOpportunityLogService {
+
+}
